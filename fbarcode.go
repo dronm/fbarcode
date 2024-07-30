99dd66d640594514db957be464f65587c0e86d9e
Andrey Mikhalevich <katrenplus@mail.ru>
2024-07-30 10:34:22 +0500

    added checksum

--- fbarcode.go
+++ fbarcode.go
@@ -127,6 +127,9 @@ func Checksum(ean string, size int) (int, error) {
 }
 
 func Ean13(barcode string) string {
+	if len(barcode) != 13 {
+		barcode = "0" + barcode
+	}
 	var res strings.Builder
 	res.WriteString(CodeChar("1111")) //start simbol
 	for i := 0; i < len(barcode)/2; i++ {
